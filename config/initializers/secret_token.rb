620794def7b9813a799ad12904ea6cc584953afc
tonyr <scuderiaf1@gmail.com>
2013-02-26 09:46:30 -0500

    Code for The Intro to Rails Screencast I Wish I Had

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '0a7dbd64f3d7438a6a6a57029e12ea38affe97c5da31d4bd18bdf94d494eaf5ae7d1e1d3735db290b7ed8898891ccbfc0e05820ea38c9e86d889f1ec6f70827c'
